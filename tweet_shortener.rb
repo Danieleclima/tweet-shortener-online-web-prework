221fe062a0a28475bfbd97d1babc7c9de22357d5
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-03-19 23:13:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,54 +1,26 @@
-
-
 def dictionary
   convert = {
-   "hello" => "hi",
-   "to" => "2",
-   "two" => "2",
-   "too" => "2",
-   "for" => "4",
-   "four" => "4",
-   "be" => "b",
-   "you" => "u",
-   "at" => "@",
-   "and" => "&"
-}
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&",
+    }
 end
 
-def word_substituter(tweet)
-  # replaces long words with their expected short form
-
-    tweet.split(" ").map do |word|
-      if dictionary.keys.include?(word.downcase)
-        word = dictionary[word.downcase]
-      else
-        word
-    end
-  end.join(" ")
-end
-
-def bulk_tweet_shortener(tweet)
-  tweet.map do |phrase|
-    puts word_substituter(phrase)
-  end
-end
-
-def selective_tweet_shortener(tweet)
-  tweet.collect do |phrase|
-    if phrase.length > 140
-      word_substituter(phrase)
-    elsif phrase.length <= 140
-      phrase
+def word_substituter (tweet)
+  tweet.split(",")
+  dictionary.each do |key, value|
+    if tweet.include?(key)
+      tweet.each do |word|
+        word == value
     end
   end
 end
-
-def shortened_tweet_truncator(tweet)
-  tweet.split(" ").map do |phrase|
-    if phrase.length > 140
-      word_substituter(phrase)[0..140] + "..."
-    else
-      phrase
-    end
-  end.join(" ")
+tweet.join(" ")
 end
